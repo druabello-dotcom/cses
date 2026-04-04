44dd0fb3ca5e4cbbdb74f1eca5e20924c28ff217
Dru C. Romano Abello <druabello@gmail.com>
2026-04-04 13:22:33 +0200

    add TLE solution for Josephus Problem 2 on test 12, 13, 15, 16

---
+++ sorting_searching/2163_josephusProblemII.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <cmath>
+
+void solve(std::list<int>& children, int n, int k) {
+	int adv = k % n;
+	int t = adv + 1;
+	auto point = std::next(children.begin(), adv);
+
+	while (0 <= n) {
+		n--;
+		auto prev = point;
+		std::cout << *point << ' ';
+		if (!n) break;
+
+		++point;
+		if (point == children.end()) point = children.begin();
+		children.erase(prev);
+
+		adv = k % n;
+		if (!adv) adv = n;
+		if (n < t) t = 1;
+		if (n < t + adv) {
+			int diff = n - t;
+			adv = abs(adv - diff - 1);
+			t = adv + 1;
+			point = std::next(children.begin(), adv);
+		} else {
+			std::advance(point, adv);
+			t += adv;
+		}
+	}
+}
+int main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	int n, k;
+	std::cin >> n >> k;
+	std::list<int> li;
+	for (int i = 1; i <= n; i++) li.push_back(i);
+
+	solve(li, n, k);
+}
